76ecf1f27ed2de836c62adc54784b11be030f080
MAHMUDUL\mahmu <mahmudulcsejnu@gmail.com>
2023-03-25 13:56:22 +0600

    Added Project Sender & Receiver

---
+++ RabbitMQDemo/RabbitReceiver1/Program.cs
@@ -0,0 +1,23 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+
+ConnectionFactory factory = new();
+factory.Uri = new Uri("amqp://guest:guest@localhost:5672");
+factory.ClientProvidedName = "Rabbit Receiver1 App";
+
+IConnection cnn = factory.CreateConnection();
+
+IModel channel = cnn.CreateModel();
+
+string exchangeName = "DemoExchange";
+string routingKey = "demo-routing-key";
+string queueName = "DemoQueue";
+
+channel.ExchangeDeclare(exchangeName, ExchangeType.Direct);
+channel.QueueDeclare(queueName, durable: false, exclusive: false, autoDelete: false, arguments: null);
+channel.QueueBind(queueName, exchangeName, routingKey, arguments: null);
+
+
+
---
+++ RabbitMQDemo/RabbitSender/Program.cs
@@ -0,0 +1,26 @@
+﻿
+using RabbitMQ.Client;
+using System.Text;
+
+ConnectionFactory factory = new();
+factory.Uri = new Uri("amqp://guest:guest@localhost:5672");
+factory.ClientProvidedName = "Rabbit Sender App";
+
+IConnection cnn = factory.CreateConnection();
+
+IModel channel = cnn.CreateModel();
+
+string exchangeName = "DemoExchange";
+string routingKey = "demo-routing-key";
+string queueName = "DemoQueue";
+
+channel.ExchangeDeclare(exchangeName, ExchangeType.Direct);
+channel.QueueDeclare(queueName, durable: false, exclusive: false, autoDelete: false, arguments: null);
+channel.QueueBind(queueName, exchangeName, routingKey, arguments: null);
+
+byte[] messageBodyBytes = Encoding.UTF8.GetBytes("Hello MQ");
+
+channel.BasicPublish(exchangeName, routingKey, basicProperties: null, messageBodyBytes);
+
+channel.Close();
+cnn.Close();
